2bf965dd81bfa7220e4ebe38a5afa9c7e458637d
ho <ho@openbsd.org>
2000-10-03 14:32:00 +0000

    Add $OpenBSD$. (jakob@ ok)

--- usr.sbin/tcpdump/bootp.h
+++ usr.sbin/tcpdump/bootp.h
@@ -1,4 +1,6 @@
-/* @(#) $Header: /usr/cvsync/src/usr.sbin/tcpdump/bootp.h,v 1.6 2000/04/26 21:35:38 jakob Exp $ (LBL) */
+/*	$OpenBSD: bootp.h,v 1.7 2000/10/03 14:31:54 ho Exp $	*/
+
+/* @(#) $Header: /usr/cvsync/src/usr.sbin/tcpdump/bootp.h,v 1.7 2000/10/03 14:31:54 ho Exp $ (LBL) */
 /*
  * Bootstrap Protocol (BOOTP).  RFC951 and RFC1048.
  *
